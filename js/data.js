707cc50eaf02dcaa39d1a416b82e736e4f1f1636
Hannah Hoang <hannahhoanggg@gmail.com>
2023-08-18 04:41:08 +0000

    Added XMLHttprequest.

--- js/data.js
+++ js/data.js
@@ -1,1 +1,19 @@
 /* exported data */
+
+let data = {
+  favHPChar: [],
+  favHPCharImg: []
+};
+
+const previousDataJSON = localStorage.getItem('harry-potter-storage');
+
+if (previousDataJSON !== null) {
+  data = JSON.parse(previousDataJSON);
+}
+
+function beforeUnload(event) {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('harry-potter-storage', dataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
